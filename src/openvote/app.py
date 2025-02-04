98827bea323697cc118f330a2dd83e2b83c60f6e
tct123 <42028373+tct123@users.noreply.github.com>
2025-02-04 20:38:20 +0100

    .

--- src/openvote/app.py
+++ src/openvote/app.py
@@ -2,7 +2,7 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 import locale
-from mylocale.TR import tr
+from mylocale import tr
 
 
 platform = toga.platform.current_platform
